82e160a058c101aa9756d1b91951c17fe2ee2bde
m020202 <m020202@naver.com>
2024-11-10 17:42:44 +0900

    Feat: adapter pattern initial commit

---
+++ src/main/java/com/pattern/design_patterns/structural_patterns/adapter/before/security/UserDetails.java
@@ -0,0 +1,7 @@
+package com.pattern.design_patterns.structural_patterns.adapter.before.security;
+
+public interface UserDetails {
+    String getUsername();
+
+    String getPassword();
+}
---
+++ src/main/java/com/pattern/design_patterns/structural_patterns/adapter/before/security/UserDetailsService.java
@@ -0,0 +1,5 @@
+package com.pattern.design_patterns.structural_patterns.adapter.before.security;
+
+public interface UserDetailsService {
+    UserDetails loadUser(String username);
+}
