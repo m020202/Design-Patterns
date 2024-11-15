75cdf174634d827e6595afaa5164997816cb8135
m020202 <m020202@naver.com>
2024-11-15 23:47:12 +0900

    Feat: builder-pattern Director EG

--- src/main/java/com/pattern/design_patterns/creational_patterns/builder/after/TourDirector.java
+++ src/main/java/com/pattern/design_patterns/creational_patterns/builder/after/TourDirector.java
@@ -4,21 +4,20 @@ import com.pattern.design_patterns.creational_patterns.builder.before.TourPlan;
 
 import java.time.LocalDate;
 
-public class App {
-    public static void main(String[] args) {
-        DefaultTourBuilder builder = new DefaultTourBuilder();
-        TourPlan plan = builder.title("칸쿤 여행")
+public class TourDirector {
+    private TourPlanBuilder tourPlanBuilder;
+
+    public TourDirector(TourPlanBuilder tourPlanBuilder) {
+        this.tourPlanBuilder = tourPlanBuilder;
+    }
+
+    public TourPlan cancunTrip() {
+        return tourPlanBuilder.title("칸쿤 여행")
                 .nightAndDays(2, 3)
                 .startDate(LocalDate.of(2020, 12, 9))
                 .whereToStay("리조트")
                 .addPlan(0, "체크인하고 짐 풀기")
                 .addPlan(0, "저녁 식사")
                 .getPlan();
-
-        TourPlanBuilder longBeachTrip = builder.title("롱비치")
-                .startDate(LocalDate.of(2021, 7, 15));
-
-        TourDirector tourDirector = new TourDirector(builder);
-        TourPlan cancunPlan = tourDirector.cancunTrip();
     }
 }
